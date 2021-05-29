b57675b15508b19522b814a0d8d364995589d53d
h.kato <cp3xzayn@gmail.com>
2021-05-29 16:18:00 +0900

    Buttonが押されたときにAnimationが発生する基底クラスを作成した。

---
+++ Assets/Script/ButtonAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    /// <summary> Button </summary>
+    [SerializeField] GameObject[] m_buttons = null;
+    /// <summary> 選択されたButton </summary>
+    GameObject m_selectedButton = null;
+
+    /// <summary>
+    /// Buttonに関数を登録する
+    /// </summary>
+    public virtual void SetButtonOnClick()
+    {
+        Debug.Log("b");
+        for (int i = 0; i < m_buttons.Length; i++)
+        {
+            int value = i; // キャプチャーのため、変数に格納する
+            m_buttons[i].GetComponent<Button>().onClick.AddListener(() => PlayButtonAnimation(value));
+        }
+    }
+
+    /// <summary>
+    /// ButtonをAnimationさせる（機能を追加する場合、overrideさせて追加する）
+    /// </summary>
+    /// <param name="i"></param>
+    public virtual void PlayButtonAnimation(int i)
+    {
+        m_selectedButton = m_buttons[i];
+        RectTransform rt = m_selectedButton.GetComponent<RectTransform>();
+        StartCoroutine(StartButtonAnimation(rt, 0.9f, 1f));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="rt"></param>
+    /// <param name="minSize"></param>
+    /// <param name="maxSize"></param>
+    /// <returns></returns>
+    IEnumerator StartButtonAnimation(RectTransform rt, float minSize, float maxSize)
+    {
+        rt.localScale = new Vector2(minSize, minSize);
+        yield return new WaitForSeconds(0.1f);
+        rt.localScale = new Vector2(maxSize, maxSize);
+        yield break;
+    }
+}
---
+++ Assets/Script/ButtonAnimationOverride.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimationOverride : ButtonAnimation
+{
+    void Start()
+    {
+        SetButtonOnClick();
+    }
+
+    public override void SetButtonOnClick()
+    {
+        base.SetButtonOnClick();
+    }
+
+    public override void PlayButtonAnimation(int i)
+    {
+        base.PlayButtonAnimation(i);
+        if (i == 1)
+        {
+            Debug.Log("c");
+        }
+    }
+}
