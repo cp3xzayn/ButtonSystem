8d1a5d25daab4c8699cee0461f3db87bb17b1205
h.kato <cp3xzayn@gmail.com>
2021-05-29 19:00:38 +0900

    Buttonのサイズ変更と音の設定を設け、Inspectorでも変更できるようにした。

--- Assets/Script/Test2/ButtonAnimationInterface.cs
+++ Assets/Script/Test2/ButtonAnimationInterface.cs
@@ -3,25 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ButtonAnimationInterface : MonoBehaviour ,
-    IPointerDownHandler, IPointerUpHandler
+[RequireComponent(typeof(AudioSource))]
+public class ButtonAnimationInterface : MonoBehaviour ,IPointerDownHandler, IPointerUpHandler
 {
+    /// <summary> ButtonのRectTransform </summary>
     RectTransform rt;
+    /// <summary> Button縮小時のサイズ </summary>
+    [Header("ボタン縮小時のサイズ"), SerializeField] float m_minSize = 0.9f;
+    /// <summary> Button拡大時のサイズ </summary>
+    [Header("ボタン拡大時のサイズ"), SerializeField] float m_maxSize = 1f;
+    /// <summary> ButtonがクリックSound </summary>
+    [Header("クリック音"), SerializeField] AudioClip m_clickSound = null;
+    /// <summary> AudioSource </summary>
+    AudioSource m_audioSource;
 
     void Start()
     {
         rt = GetComponent<RectTransform>();
+        m_audioSource = GetComponent<AudioSource>();
     }
 
+    /// <summary>
+    /// Button押下時の処理
+    /// </summary>
+    /// <param name="pointerEventData"></param>
     public virtual void OnPointerDown(PointerEventData pointerEventData)
     {
-        Debug.Log("a");
-        rt.localScale = new Vector2(0.9f, 0.9f);
+        Debug.Log("ボタンが押されました。");
+        rt.localScale = new Vector2(m_minSize, m_minSize);
+        m_audioSource.PlayOneShot(m_clickSound);
     }
 
+    /// <summary>
+    /// Buttonから離れたときの処理
+    /// </summary>
+    /// <param name="eventData"></param>
     public virtual void OnPointerUp(PointerEventData eventData) 
     {
-        Debug.Log("b");
-        rt.localScale = new Vector2(1f, 1f);
+        Debug.Log("ボタンから離れました。");
+        rt.localScale = new Vector2(m_maxSize, m_maxSize);
     }
 }
